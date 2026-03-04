c38f95ec68c86a4117f76736781b25fcf5a420e8
Kratarth Srivastava <srivastava.magnificent@gmail.com>
2026-03-04 23:23:38 +0530

    Update live_detect.py

--- live_detect.py
+++ live_detect.py
@@ -7,7 +7,7 @@ from email.message import EmailMessage
 from ultralytics import YOLO
 
 # ---------------- MODELS ----------------
-weapon_model = YOLO("runs/detect/train4/weights/best.pt")
+weapon_model = YOLO("best.pt")
 person_model = YOLO("yolov8n.pt")
 
 # ---------------- SETTINGS ----------------
